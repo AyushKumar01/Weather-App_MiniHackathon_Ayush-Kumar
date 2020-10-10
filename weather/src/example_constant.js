cf8d04df32f3a7f7998b75c62ac2381be719bb3c
AyushKumar01 <chauhan01ayush@gmail.com>
2020-10-10 15:15:55 -0700

    added example_constant.js

---
+++ weather/src/example_constant.js
@@ -0,0 +1,5 @@
+// I used 'open weather map' website to get weather data
+// It's a free website for one point
+// https://api.openweathermap.org
+export const API_KEY = "Your_Key";
+export const URL = "Your_Url"
