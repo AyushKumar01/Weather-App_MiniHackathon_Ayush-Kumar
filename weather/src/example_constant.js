758b83a74a8a2bd23c9f6a7f3fb3ba03457aa1df
AyushKumar01 <chauhan01ayush@gmail.com>
2020-10-27 21:41:18 -0700

    constant.js updated

--- weather/src/example_constant.js
+++ weather/src/example_constant.js
@@ -2,4 +2,6 @@
 // It's a free website for one point
 // https://api.openweathermap.org
 export const API_KEY = "Your_Key";
-export const SEARCH_URL = "Your_Url"
+export const SEARCH_URL = "Your_Url";
+export const URL = "https://api.openweathermap.org/data/2.5/weather?q=";
+export const ICON_URL = "https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/";
